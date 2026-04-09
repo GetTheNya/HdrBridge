2882fd8d023cfaf42af7e954758649a8cb4ccf00
GetTheNya <romaro422@gmail.com>
2026-04-09 19:32:49 +0300

    Control brightness from tray.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,8 @@
+using HdrBridge.ViewModels;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Threading;
-using HdrBridge.ViewModels;
+using Xceed.Wpf.AvalonDock.Controls;
 
 namespace HdrBridge;
 
@@ -32,6 +33,12 @@ public partial class MainWindow : Window {
             SyncToggleMenuItem.Header = vm.IsServicesEnabled ? "Disable Sync" : "Enable Sync";
     }
 
+    private void TrayIcon_TrayLeftMouseUp(object sender, RoutedEventArgs e) {
+        if (DataContext is MainViewModel vm) {
+            _ = vm.ToggleServices();
+        }
+    }
+
     private void TrayIcon_TrayMouseDoubleClick(object sender, RoutedEventArgs e) {
         Show();
         if (WindowState == WindowState.Minimized) WindowState = WindowState.Normal;
@@ -47,6 +54,58 @@ public partial class MainWindow : Window {
     private void StartupCheckBox_Changed(object sender, RoutedEventArgs e) {
         if (DataContext is MainViewModel vm)
             vm.SaveStartupPreference();
+    }
+
+    private void Brightness_Click(object sender, RoutedEventArgs e) {
+        var selectedItem = sender as MenuItem;
+
+        ClearAllBrightnessCheckmarks(selectedItem);
+
+        selectedItem.IsChecked = true;
+
+        string tagValue = selectedItem.Tag?.ToString();
+
+        if (DataContext is MainViewModel vm) {
+            int brightness = vm.HyperHdrMaxBrightness;
+
+            switch (tagValue) {
+                case "0": brightness = 100;  break;
+                case "1": brightness = 75; break;
+                case "2": brightness = 50;  break;
+                case "3": brightness = 25; break;
+            }
+
+            vm.HyperHdrMaxBrightness = brightness;
+        }
+    }
+
+    private void Brightness_Change(object sender, RoutedEventArgs e) {
+        var selectedItem = sender as MenuItem;
+
+        int step = (selectedItem.Tag.ToString() == "0") ? 10 : -10;
+
+        if (DataContext is MainViewModel vm) {
+            int brightness = vm.HyperHdrMaxBrightness;
+
+            brightness += step;
+
+            if (brightness > 100) brightness = 100;
+            if (brightness < 5) brightness = 5;
+
+            vm.HyperHdrMaxBrightness = brightness;
+
+            ClearAllBrightnessCheckmarks(selectedItem);
+        }
+    }
+
+    private void ClearAllBrightnessCheckmarks(MenuItem currentItem) {
+        if (currentItem.Parent is MenuItem parentMenu) {
+            foreach (var item in parentMenu.Items) {
+                if (item is MenuItem mi) {
+                    mi.IsChecked = false;
+                }
+            }
+        }
     }
 
     private void MenuItem_Exit_Click(object sender, RoutedEventArgs e) {
